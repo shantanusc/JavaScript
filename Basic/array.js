74aae83595cb92f5f87042c0084a6aaa249a924d
shantanusc <152727181+shantanusc@users.noreply.github.com>
2024-06-24 18:42:04 +0000

    over

---
+++ Basic/array.js
@@ -0,0 +1,29 @@
+let ar = [23, "shant", "shantanu", 89]
+console.log(ar);
+console.log(ar[2]);
+// interview 
+// shollow copies and deep copies
+// console.log(length(ar));
+
+// array method()
+
+const arr = [1, 2, 3, 4, 5]
+console.log(arr.includes(3));
+console.log(typeof arr.includes(3));
+console.log(typeof arr);
+console.log(arr.join());
+console.log(typeof arr.join());
+
+
+console.log(arr.pop());
+console.log(arr.push(34));
+console.log(arr);
+
+
+// array inside an array
+let arr2 = ["shantnau", "vaibava", "shanu"]
+let arr3 = ["nane", "hello", "nonehello"]
+
+arr2.push(arr3);
+console.log(arr3);
+
--- Basic/test.js
+++ Basic/test.js
@@ -1,2 +1,20 @@
 console.log("hello");
 console.log("vas");
+let myDate = new Date();
+console.log(myDate);
+
+
+// date
+console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+console.log(typeof myDate );
+
+let mytime = Date.now()
+
+console.log(mytime);
+console.log(Math.floor(Date.now()));
+
+let newDate  = new Date()
+console.log(newDate);
+console.log(newDate.getFullYear());
+console.log(newDate.getMonth());
